376040daae5ea565253212abe411e15c7e7a2dfc
MagUbas <magdalena.ubas@gmail.com>
2023-09-15 12:11:28 +0200

    styling and improving Task component

--- src/Components/Task/index.js
+++ src/Components/Task/index.js
@@ -1,5 +1,34 @@
+import classes from "./index.module.css";
+import { IconContext } from "react-icons";
+import { BiPencil, BiTrash } from "react-icons/bi";
+
 function Task(props) {
-  return <li disabled={props.complate}>{props.text}</li>;
+  return (
+    <li className={classes.task} disabled={props.complate}>
+      {/* jak zrobic,zeby wyswietlalo sie na key on title */}
+      {/* <div className={classes.taskInputGroup}>
+       
+        <label for={props.text}>{props.text}</label>
+      </div> */}
+      <label className={classes.taskInputGroup} for={props.text}>
+        <input type="checkbox" id={props.text}></input>
+        <span className={classes.taskInputGroupCheckmark}></span>
+
+        {props.text}
+      </label>
+
+      <div className={classes.taskButtonGroup}>
+        <IconContext.Provider value={{ color: "white", size: "1.7rem" }}>
+          <button title="Edit">
+            <BiPencil />
+          </button>
+          <button title="Delete">
+            <BiTrash />
+          </button>
+        </IconContext.Provider>
+      </div>
+    </li>
+  );
 }
 
 export default Task;
--- src/Components/TaskGroup/index.js
+++ src/Components/TaskGroup/index.js
@@ -1,10 +1,11 @@
 import Task from "../Task/index";
+import classes from "./index.module.css";
 
 function TaskGroup(props) {
   return (
-    <div>
+    <div className={classes.taskGroup}>
       <h3>{props.date}</h3>
-      <ul>
+      <ul className={classes.taskList}>
         {props.taskList.map((elem) => {
           return (
             <Task key={elem.text} text={elem.text} complate={elem.complate} />
